fe7be3dd470c77c367598e64f8ae924812c85d96
23raven <23raven.games@gmail.com>
2026-04-08 22:33:49 +0500

    add board

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -3,9 +3,29 @@ using UnityEngine.UI;
 
 public class Cell : MonoBehaviour
 {
+    public int index;
+
     public Sprite xSprite;
     public Sprite oSprite;
 
-    public int index;
+    private Image image;
+    private Button button;
 
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+        button = GetComponent<Button>();
+
+        button.onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        SetX();
+    }
+
+    void SetX()
+    {
+        image.sprite = xSprite;
+    }
 }
